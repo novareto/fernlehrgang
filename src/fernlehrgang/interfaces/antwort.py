cf84bf1fa4373889290ba7b71ebe0473df609e53
goschtl <goschtl@d44c204c-eeda-11de-b92f-5d3588262692>
2010-01-28 15:54:08 +0000

    Added Antwort for Kursteilnehmer

---
+++ src/fernlehrgang/interfaces/antwort.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2008 NovaReto GmbH
+# cklinger@novareto.de 
+
+import grok
+
+from zope.schema import *
+from zope.interface import Interface
+
+class IAntwort(Interface):
+
+    id = Int(
+        title = u'Id',
+        description = u'Eindeutige Kennzeichnung der Antwort',
+        required = False,
+        readonly = True
+        )
+
+    frage = Int(
+        title = u'Frage',
+        description = u'Für welche Frage soll das Antwortschema sein.',
+        required = True,
+        )
+
+    antwortschema = TextLine(
+        title = u'Antwortschema',
+        description = u'Bitte geben Sie Antwortmöglichkeiten ein.',
+        required = True,
+        )
+
