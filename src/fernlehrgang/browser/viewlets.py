51919e5a185139101ba3cdbc2e61458115d9178d
goschtl <goschtl@d44c204c-eeda-11de-b92f-5d3588262692>
2010-02-05 16:11:39 +0000

    Now nice GlobalMenu with FLGS

---
+++ src/fernlehrgang/browser/viewlets.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2010 NovaReto GmbH
+# cklinger@novareto.de 
+
+import grok
+
+from z3c.saconfig import Session
+from fernlehrgang.models import Fernlehrgang
+from zope.interface import Interface
+from uvc.layout.interfaces import IGlobalMenu
+
+
+
+class GlobalMenu(grok.ViewletManager):
+    grok.name('uvcsite.globalmenu')
+    grok.context(Interface)
+    grok.implements(IGlobalMenu)
+    template = grok.PageTemplateFile('globalmenu.pt')
+
+    css = ['blue', 'orange', 'violet', 'green', 'brown', 'purple']
+
+    def getClass(self, index):
+        return self.css[index]
+
+    @property
+    def flgs(self):
+        session = Session()
+        rc = []
+        i=0
+        for fernlehrgang in session.query(Fernlehrgang).all():
+            url = "%s/fernlehrgang/%s" %(self.view.url(), fernlehrgang.id)
+            rc.append(dict(title=fernlehrgang.jahr, css=self.css[i], url=url))
+            i+=1 # ENUMERATE
+        return rc    
+
+
+
