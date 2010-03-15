d11c7dbc20433a4aa5fc6ef13b5238edf4c56d93
goschtl <goschtl@d44c204c-eeda-11de-b92f-5d3588262692>
2010-03-15 09:56:55 +0000

    Cleanups

--- fernlehrgang/trunk/src/fernlehrgang/browser/ergebnisse.py
+++ fernlehrgang/trunk/src/fernlehrgang/browser/ergebnisse.py
@@ -8,9 +8,8 @@ from grok import url, getSite
 from z3c.saconfig import Session
 from megrok.traject import locate
 from dolmen.menu import menuentry
-from fernlehrgang.utils import Page
+from megrok.layout import Page
 from fernlehrgang.models import Antwort, Frage 
-from fernlehrgang.utils import MenuItem 
 from uvc.layout.interfaces import ISidebar
 from fernlehrgang.interfaces.antwort import IAntwort
 from megrok.traject.components import DefaultModel
