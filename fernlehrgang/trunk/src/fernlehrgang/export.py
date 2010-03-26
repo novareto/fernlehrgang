904204b02c21bac14166c5b4ae3a146876493db4
goschtl <goschtl@d44c204c-eeda-11de-b92f-5d3588262692>
2010-03-26 16:08:00 +0000

    export tests etc...

--- fernlehrgang/trunk/src/fernlehrgang/export.py
+++ fernlehrgang/trunk/src/fernlehrgang/export.py
@@ -50,10 +50,60 @@ class XMLExport(grok.Adapter):
         jahr = SubElement(fernlehrgang, 'jahr')
         jahr.text = str(context.jahr)
 
+        # Zweig 1 Lehrhefte...
+
         for lehrheft in context.lehrhefte:
-            f_elem = SubElement(fernlehrgang, 'lehrheft')
-            f_elem.attrib['lehrheft_id'] = str(notNone(lehrheft.id))
+            l_elem = SubElement(fernlehrgang, 'lehrheft')
+            l_elem.attrib['lehrheft_id'] = str(notNone(lehrheft.id))
+
+            for frage in lehrheft.fragen:
+                f_elem = SubElement(l_elem, 'frage')
+                f_elem.attrib['frageid'] = str(notNone(frage.id))
+                loesung = SubElement(f_elem, 'loesung')
+                loesung.text = frage.antwortschema
+                maxpunkte = SubElement(f_elem, 'maxpunkte')
+                maxpunkte.text = str(frage.gewichtung)
             
+
+        # Zweig 2 Kursteilnehmer...
+
+        for kursteilnehmer in context.kursteilnehmer:
+            teilnehmer = kursteilnehmer.teilnehmer
+            k_elem = SubElement(fernlehrgang, 'kursteilnehmer')
+            k_elem.attrib['ktlnid'] = str(notNone(kursteilnehmer.id))
+            t_elem = SubElement(k_elem, 'teilnehmer')
+            t_elem.attrib['tlnid'] = str(notNone(teilnehmer.id))
+
+            #Personendaten
+            SubElement(t_elem, 'anrede').text = teilnehmer.anrede
+            SubElement(t_elem, 'titel').text = teilnehmer.titel
+            SubElement(t_elem, 'name').text = teilnehmer.name
+            SubElement(t_elem, 'vorname').text = teilnehmer.vorname
+            SubElement(t_elem, 'gebdat').text = teilnehmer.geburtsdatum
+            SubElement(t_elem, 'email').text = teilnehmer.email
+            SubElement(t_elem, 'telpriv').text = ''
+            SubElement(t_elem, 'teldienst').text = '' 
+            SubElement(t_elem, 'telmobil').text = '' 
+            SubElement(t_elem, 'telefax').text = ''
+
+            #Unternehmen
+            unternehmen = teilnehmer.unternehmen
+            u_elem = SubElement(t_elem, 'unternehmen')
+            u_elem.attrib['unternehmensid'] = unternehmen.mnr
+            SubElement(u_elem, 'name1').text = unternehmen.name 
+            SubElement(u_elem, 'name2').text = unternehmen.name2 
+            SubElement(u_elem, 'name3').text = unternehmen.name3 
+            SubElement(u_elem, 'strhnr').text = unternehmen.str
+            SubElement(u_elem, 'plz').text = unternehmen.plz
+            SubElement(u_elem, 'ort').text = unternehmen.ort
+
+            #Versandanschrift
+            v_elem = SubElement(t_elem, 'versandanschrift')
+            SubElement(v_elem, 'name1').text = notNone(teilnehmer.anrede) + ' ' + notNone(teilnehmer.titel)
+            SubElement(v_elem, 'name2').text = notNone(teilnehmer.name) + ' ' + notNone(teilnehmer.vorname)
+            SubElement(v_elem, 'strhnr').text = notNone(teilnehmer.strasse) + ' ' + notNone(teilnehmer.nr)
+            SubElement(v_elem, 'plz').text = teilnehmer.plz
+            SubElement(v_elem, 'ort').text = teilnehmer.ort
 
 
         return prettify(root)
