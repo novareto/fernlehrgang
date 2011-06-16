ea372afd2032f8356de38629bbf83ef00e029efc
goschtl <goschtl@d44c204c-eeda-11de-b92f-5d3588262692>
2011-06-16 14:11:46 +0000

    starting export

--- src/fernlehrgang/export.py
+++ src/fernlehrgang/export.py
@@ -2,121 +2,152 @@
 # Copyright (c) 2007-2010 NovaReto GmbH
 # cklinger@novareto.de 
 
-import grok
-from xml.etree.cElementTree import ElementTree, Element, SubElement, tostring 
-from zope.interface import Interface
+import grokcore.component as grok
 
 from fernlehrgang.interfaces.flg import IFernlehrgang
+from fernlehrgang.interfaces.resultate import ICalculateResults
+from xlwt import Workbook
+from zope.interface import Interface
+from zope.schema import *
+from zope.schema.interfaces import IVocabularyFactory, IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-class IXMLExport(Interface):
+
+
+@grok.provider(IContextSourceBinder)
+def lehrhefte(context):
+    rc = []
+    for lehrheft in context.lehrhefte:
+        titel = "%s, %s" %(lehrheft.nummer, lehrheft.titel)
+        rc.append(SimpleTerm(lehrheft.id, lehrheft.id, titel))
+    return SimpleVocabulary(rc)
+
+
+class IXLSExport(Interface):
     """ xml export """
 
-    def createXML():
+    dateiname = TextLine(
+        title=u"Dateiname",
+        description=u"Dateiname"
+        )
+
+    rdatum = TextLine(
+        title=u"RDatum",
+        description=u"RDatum"
+        )
+
+    stichtag = TextLine(
+        title=u"Stichtag",
+        description=u"Stichtag"
+        )
+
+    lehrheft = Choice(
+        title=u"Lehrheft",
+        description=u"Lehrheft",
+        source = lehrhefte,
+        )
+
+
+    def createXLS():
         """ """
 
-from xml.etree import ElementTree
-from xml.dom import minidom
-
-def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
-    rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
 
 
-def notNone(value):
+spalten = ('FLG_ID', 'TEILNEHMER_ID', 'LEHRHEFT_ID', 'PLZ', 'MITGLNRMIT', 'FIRMA', 'ANREDE', 'TITEL', 'VORNAME', 'NAME',
+    'STRASSE', 'WOHNORT', 'PASSWORT', 'BELIEFART', 'R_DATUM', 'RSENDUNG', 'PUNKTZAHL',
+    'STICHTAG', 'LEHRHEFT', 'R_TITEL', 'R_VORNAME', 'R_NAME', 'L1_F_1', 'L1_F_2',
+    'L1_F_3', 'L1_F_4', 'L1_F_5', 'L1_F_6', 'L1_F_7', 'L1_F_8', 'L1_F_9', 'L1_F_10',
+    'L2_F_1', 'L2_F_2', 'L2_F_3', 'L2_F_4', 'L2_F_5', 'L2_F_6', 'L2_F_7', 'L2_F_8', 'L2_F_9',
+    'L2_F_10', 'L3_F_1', 'L3_F_2', 'L3_F_3', 'L3_F_4', 'L3_F_5', 'L3_F_6', 'L3_F_7',
+    'L3_F_8', 'L3_F_9', 'L3_F_10', 'L4_F_1', 'L4_F_2', 'L4_F_3', 'L4_F_4', 'L4_F_5',
+    'L4_F_6', 'L4_F_7', 'L4_F_8', 'L4_F_9', 'L4_F_10','L5_F_1', 'L5_F_2', 'L5_F_3',
+    'L5_F_4', 'L5_F_5', 'L5_F_6', 'L5_F_7', 'L5_F_8', 'L5_F_9', 'L5_F_10', 'L6_F_1',
+    'L6_F_2', 'L6_F_3', 'L6_F_4', 'L6_F_5', 'L6_F_6', 'L6_F_7', 'L6_F_8', 'L6_F_9',
+    'L6_F_10', 'L7_F_1', 'L7_F_2', 'L7_F_3', 'L7_F_4', 'L7_F_5', 'L7_F_6', 'L7_F_7',
+    'L7_F_8', 'L7_F_9', 'L7_F_10', 'L8_F_1', 'L8_F_2', 'L8_F_3', 'L8_F_4', 'L8_F_5',
+    'L8_F_6', 'L8_F_7', 'L8_F_8', 'L8_F_9', 'L8_F_10','L1_P', 'L2_P', 'L3_P',
+    'L4_P', 'L5_P', 'L6_P', 'L7_P', 'L8_P', 'L9_P', 'L10_P'
+)
+
+
+
+def nN(value):
     """ Not None"""
     if value == None:
         return ''
     return value
 
+RDATUM = "01.12.2010"
+STICHTAG = "30.12.2010"
+LEHRHEFT_ID = "222"
 
-class XMLExport(grok.Adapter):
+
+class XLSExport(grok.Adapter):
     """ XML Export"""
-    grok.implements(IXMLExport)
+    grok.implements(IXLSExport)
     grok.context(IFernlehrgang)
 
-    def createXML(self):
-        context = self.context
+    def __init__(self, context):
+        self.context = context
+        self.book = Workbook()
+        self.adressen = self.book.add_sheet('FLG-XLS Export')
+         
+    def createSpalten(self):
+        for i, spalte in enumerate(spalten):
+            self.adressen.write(0, i, spalte)
 
-        root = Element("xml")
-        lehrgaenge = SubElement(root, 'lehrgaenge')
-
-        fernlehrgang = SubElement(lehrgaenge, 'fernlehrgang')
-        fernlehrgang.attrib['flgid'] = str(notNone(context.id))
-        fernlehrgang.attrib['titel'] = context.titel
-
-        jahr = SubElement(fernlehrgang, 'jahr')
-        jahr.text = str(context.jahr)
-
-        # Zweig 1 Lehrhefte...
-
-        for lehrheft in context.lehrhefte:
-            l_elem = SubElement(fernlehrgang, 'lehrheft')
-            l_elem.attrib['lehrheft_id'] = str(notNone(lehrheft.id))
-
-            for frage in lehrheft.fragen:
-                f_elem = SubElement(l_elem, 'frage')
-                f_elem.attrib['frageid'] = str(notNone(frage.id))
-                loesung = SubElement(f_elem, 'loesung')
-                loesung.text = frage.antwortschema
-                maxpunkte = SubElement(f_elem, 'maxpunkte')
-                maxpunkte.text = str(frage.gewichtung)
-            
-
-        # Zweig 2 Kursteilnehmer...
-
-        for kursteilnehmer in context.kursteilnehmer:
-            teilnehmer = kursteilnehmer.teilnehmer
-            k_elem = SubElement(fernlehrgang, 'kursteilnehmer')
-            k_elem.attrib['ktlnid'] = str(notNone(kursteilnehmer.id))
-            t_elem = SubElement(k_elem, 'teilnehmer')
-            t_elem.attrib['tlnid'] = str(notNone(teilnehmer.id))
-
-            #Personendaten
-            SubElement(t_elem, 'anrede').text = teilnehmer.anrede
-            SubElement(t_elem, 'titel').text = teilnehmer.titel
-            SubElement(t_elem, 'name').text = teilnehmer.name
-            SubElement(t_elem, 'vorname').text = teilnehmer.vorname
-            SubElement(t_elem, 'gebdat').text = teilnehmer.geburtsdatum
-            SubElement(t_elem, 'email').text = teilnehmer.email
-            SubElement(t_elem, 'telpriv').text = ''
-            SubElement(t_elem, 'teldienst').text = '' 
-            SubElement(t_elem, 'telmobil').text = '' 
-            SubElement(t_elem, 'telefax').text = ''
-
-            #Unternehmen
+    def createRows(self, form):
+        flg = self.context
+        for i, ktn in enumerate(self.context.kursteilnehmer):
+            cal_res = ICalculateResults(ktn)
+            summary = cal_res.summary()
+            row = self.adressen.row(i+1)
+            teilnehmer = ktn.teilnehmer
             unternehmen = teilnehmer.unternehmen
-            u_elem = SubElement(t_elem, 'unternehmen')
-            u_elem.attrib['unternehmensid'] = unternehmen.mnr
-            SubElement(u_elem, 'name1').text = unternehmen.name 
-            SubElement(u_elem, 'name2').text = unternehmen.name2 
-            SubElement(u_elem, 'name3').text = unternehmen.name3 
-            SubElement(u_elem, 'strhnr').text = unternehmen.str
-            SubElement(u_elem, 'plz').text = unternehmen.plz
-            SubElement(u_elem, 'ort').text = unternehmen.ort
+            row.write(0, nN(flg.id))
+            row.write(1, nN(teilnehmer.id))
+            row.write(2, form['lehrheft'])
+            row.write(3, nN(teilnehmer.plz or unternehmen.plz))
+            row.write(4, nN(unternehmen.mnr))
+            row.write(5, nN(unternehmen.name))
+            row.write(6, nN(teilnehmer.anrede))
+            row.write(7, nN(teilnehmer.titel))
+            row.write(8, nN(teilnehmer.vorname))
+            row.write(9, nN(teilnehmer.name))
+            row.write(10, nN(teilnehmer.strasse) + ' '  + nN(teilnehmer.nr) or nN(unternehmen.str))
+            row.write(11, nN(teilnehmer.ort or unternehmen.ort))
+            row.write(12, nN(teilnehmer.passwort))
+            row.write(13, '') # Beliefart --> Leer laut Frau Esche 
+            row.write(14, form['rdatum']) # Variable
+            row.write(15, '2') # RSENDUNG --> Anzahl der Rücksendung
+            row.write(16, summary.get('resultpoints')) # PUNKTZAHL --> Punktzahl der Rücksendungen
+            row.write(17, form['stichtag']) # Variable
+            row.write(18, '3') # LEHRHEFT --> Variable Für Welchen Ausdruck
+            row.write(19, nN(teilnehmer.titel))
+            row.write(20, nN(teilnehmer.vorname))
+            row.write(21, nN(teilnehmer.name))
+            z = 22 
+            for lehrheft in flg.lehrhefte:
+                for frage in sorted(lehrheft.fragen, key=lambda frage: int(frage.frage)):
+                    for antwort in ktn.antworten:
+                        if frage.id == antwort.frage_id:
+                            r = "%s %s %s" %(
+                                    frage.antwortschema.upper(), 
+                                    antwort.antwortschema, 
+                                    cal_res.calculateResult(
+                                        frage.antwortschema,
+                                        antwort.antwortschema,
+                                        frage.gewichtung))
+                    row.write(z, r) 
+                    z += 1
+            for lhr in cal_res.lehrhefte():
+                row.write(z, lhr.get('punkte'))
+                z += 1
 
-            #Versandanschrift
-            v_elem = SubElement(t_elem, 'versandanschrift')
-            SubElement(v_elem, 'name1').text = notNone(teilnehmer.anrede) + ' ' + notNone(teilnehmer.titel)
-            SubElement(v_elem, 'name2').text = notNone(teilnehmer.name) + ' ' + notNone(teilnehmer.vorname)
-            SubElement(v_elem, 'strhnr').text = notNone(teilnehmer.strasse) + ' ' + notNone(teilnehmer.nr)
-            SubElement(v_elem, 'plz').text = teilnehmer.plz
-            SubElement(v_elem, 'ort').text = teilnehmer.ort
-
-            #Antworten
-            a_elem = SubElement(t_elem, 'antworten')
-            antworten = kursteilnehmer.antworten
-            for antwort in antworten:
-                an_elem = SubElement(a_elem, 'antwort')
-                an_elem.attrib['antwortid'] = str(antwort.id)
-                an_elem.attrib['lehrheftid'] = str(antwort.lehrheft_id) 
-                an_elem.attrib['frageid'] = str(antwort.frage_id)
-                SubElement(an_elem, 'loesung').text = notNone(antwort.antwortschema)
-                SubElement(an_elem, 'maxpunkte').text = notNone(antwort.frage.gewichtung)
-
-
-        return prettify(root)
-
-
+    def createXLS(self, form):
+        print form 
+        self.createSpalten()
+        self.createRows(form)
+        file = open('/tmp/adr.xls', 'w+')
+        self.book.save(file)
+        return file
