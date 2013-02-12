5c946d09a721ae8186184aaeef32566c2c53f914
ck <ck@novareto.de>
2013-02-12 14:46:36 +0000

    fortbildung liste

--- src/fernlehrgang/lib/fortbildung.py
+++ src/fernlehrgang/lib/fortbildung.py
@@ -26,10 +26,23 @@ class XLSFortbildung(grok.Adapter):
     grok.implements(IXLSFortbildung)
     grok.provides(IXLSFortbildung)
     grok.context(IFernlehrgang)
-    spalten = ['TEILNEHMER_ID', 'Titel', 'Anrede', 'Name', 'Vorname', 'Geburtsdatum', 'Strasse', 'Hausnummer', 'PLZ', 'ORT',
-        'Mitgliedsnummer', 'Unternehmen', ' ', ' ', 'Strasse', 'PLZ', 'Ort', 'Registriert', 'Kategorie', 'Lieferstopps',
-        'Mitarbeiteranzahl', 'Branche (Schrotthandel etc..)', u'Abschlussgespräch', 'Status', 'Punktzahl',
-        u'Antwortbögen']
+
+    spalten = ['FLG_ID', 'TEILNEHMER_ID', 'LEHRHEFT_ID', 'VERSANDANSCHRIFT', 'PLZ',
+        'MITGLNRMIT', 'FIRMA', 'FIRMA2', 'ANREDE', 'TITEL', 'VORNAME', 'NAME', 'GEBURTSDATUM',
+        'STRASSE', 'WOHNORT', 'PASSWORT', 'BELIEFART', 'R_DATUM', 'RSENDUNG', 'PUNKTZAHL',
+        'STICHTAG', 'LEHRHEFT', 'R_TITEL', 'R_VORNAME', 'R_NAME', 'L1_F_1', 'L1_F_2',
+        'L1_F_3', 'L1_F_4', 'L1_F_5', 'L1_F_6', 'L1_F_7', 'L1_F_8', 'L1_F_9', 'L1_F_10',
+        'L2_F_1', 'L2_F_2', 'L2_F_3', 'L2_F_4', 'L2_F_5', 'L2_F_6', 'L2_F_7', 'L2_F_8', 'L2_F_9',
+        'L2_F_10', 'L3_F_1', 'L3_F_2', 'L3_F_3', 'L3_F_4', 'L3_F_5', 'L3_F_6', 'L3_F_7',
+        'L3_F_8', 'L3_F_9', 'L3_F_10', 'L4_F_1', 'L4_F_2', 'L4_F_3', 'L4_F_4', 'L4_F_5',
+        'L4_F_6', 'L4_F_7', 'L4_F_8', 'L4_F_9', 'L4_F_10','L5_F_1', 'L5_F_2', 'L5_F_3',
+        'L5_F_4', 'L5_F_5', 'L5_F_6', 'L5_F_7', 'L5_F_8', 'L5_F_9', 'L5_F_10', 'L6_F_1',
+        'L6_F_2', 'L6_F_3', 'L6_F_4', 'L6_F_5', 'L6_F_6', 'L6_F_7', 'L6_F_8', 'L6_F_9',
+        'L6_F_10', 'L7_F_1', 'L7_F_2', 'L7_F_3', 'L7_F_4', 'L7_F_5', 'L7_F_6', 'L7_F_7',
+        'L7_F_8', 'L7_F_9', 'L7_F_10', 'L8_F_1', 'L8_F_2', 'L8_F_3', 'L8_F_4', 'L8_F_5',
+        'L8_F_6', 'L8_F_7', 'L8_F_8', 'L8_F_9', 'L8_F_10','L1_P', 'L2_P', 'L3_P',
+        'L4_P', 'L5_P', 'L6_P', 'L7_P', 'L8_P', 'L9_P', 'L10_P', 'BDANZSDG', 'BDNR', 'BDGEWICHT',
+        'BZANZSDG', 'BZANZBD', 'BDANFANG', 'BDENDE',] 
 
     def __init__(self, context):
         self.context = context
@@ -58,22 +71,87 @@ class XLSFortbildung(grok.Adapter):
     def createRows(self, data):
         session = Session()
         ids = [x for x in data['fortbildungen']]
-        result = session.query(models.Teilnehmer, models.Kursteilnehmer, models.Unternehmen).filter(
+        result = session.query(models.Teilnehmer, models.Unternehmen, models.Kursteilnehmer).filter(
             and_(
                 models.Kursteilnehmer.fernlehrgang_id.in_(ids),
                 models.Antwort.datum > data['stichtag'],
                 models.Antwort.kursteilnehmer_id == models.Kursteilnehmer.id,
                 models.Kursteilnehmer.teilnehmer_id == models.Teilnehmer.id,
                 models.Teilnehmer.unternehmen_mnr == models.Unternehmen.mnr)).order_by(models.Teilnehmer.id)
+        i=1
         print result.count()
-        for x, y, z in result.all():
-            print x, y, z
-            print "-"*55
+        for teilnehmer, unternehmen, ktn in result.all():
+            print i
+            cal_res = ICalculateResults(ktn)
+            summary = cal_res.summary()
+            liste = []
+            unternehmen = teilnehmer.unternehmen
+            ss = set([x.lehrheft_id for x in ktn.antworten])
+            antworten = len(ss)
+            if teilnehmer and summary.get('resultpoints') >= 1:
+                gebdat = ""
+                if teilnehmer.geburtsdatum:
+                    try:
+                        gebdat = teilnehmer.geburtsdatum.strftime('%d.%m.%Y')
+                    except:
+                        gebdat = ""
+                #unternehmen = teilnehmer.unternehmen
+                liste.append(nN(ktn.fernlehrgang_id))
+                liste.append(nN(teilnehmer.id))
+                liste.append(nN(ktn.fernlehrgang.lehrhefte[0].id)) # IST IMMER NUR EINS DA--Fortbilding
+                liste.append(nN(self.versandanschrift(teilnehmer)))
+                liste.append(nN(teilnehmer.plz or unternehmen.plz))
+                liste.append(nN(unternehmen.mnr))
+                liste.append(nN(unternehmen.name))
+                liste.append(nN(unternehmen.name2))
+                liste.append(nN(teilnehmer.anrede))
+                liste.append(nN(teilnehmer.titel))
+                liste.append(nN(teilnehmer.vorname))
+                liste.append(nN(teilnehmer.name))
+                liste.append(nN(gebdat))
+                strasse = nN(teilnehmer.strasse) + ' ' + nN(teilnehmer.nr)
+                if strasse == " ":
+                    strasse = nN(unternehmen.str)
+                else:
+                    if teilnehmer.adresszusatz:
+                        strasse = strasse + ' // ' + teilnehmer.adresszusatz
+                liste.append(nN(strasse))
+                liste.append(nN(teilnehmer.ort or unternehmen.ort))
+                liste.append(nN(teilnehmer.passwort))
+                liste.append(' ')
+                liste.append(' ') # RDATUM
+                liste.append(' ') # ANZAHL 
+                liste.append(nN(summary.get('resultpoints')))
+                liste.append(nN(' ')) # STICHTAG
+                liste.append(nN(ktn.fernlehrgang.lehrhefte[0].id))
+                liste.append(nN(teilnehmer.titel))
+                liste.append(nN(teilnehmer.vorname))
+                liste.append(nN(teilnehmer.name))
+                for lehrheft in ktn.fernlehrgang.lehrhefte:
+                    for frage in sorted(lehrheft.fragen, key=lambda frage: int(frage.frage)):
+                        r=""
+                        for antwort in ktn.antworten:
+                            if frage.id == antwort.frage_id:
+                                r = "%s\r %s\n %s\r" %(
+                                        frage.antwortschema.upper(),
+                                        nN(antwort.antwortschema),
+                                        cal_res.calculateResult(
+                                            frage.antwortschema,
+                                            antwort.antwortschema,
+                                            frage.gewichtung))
+                        liste.append(r)
+            self.rc.append(liste)
+            i+=1
+
+    def versandanschrift(self, teilnehmer):
+        if teilnehmer.strasse != None or teilnehmer.nr != None or teilnehmer.plz != None or teilnehmer.ort != None:
+            return "JA"
+        return ""
 
     def create(self, data):
         self.createRows(data)
-        #for z, line in enumerate(self.rc):
-        #    self.adressen.append([cell for cell in line])
-        #file = '/tmp/report.xls'
-        #self.book.save(file)
-        #return open(file, 'r')
+        for z, line in enumerate(self.rc):
+            self.adressen.append([cell for cell in line])
+        file = '/tmp/fortbildung.xls'
+        self.book.save(file)
+        return open(file, 'r')
