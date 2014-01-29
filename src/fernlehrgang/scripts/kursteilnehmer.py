5209151275df3b1ceabbe278ca8a459a4cc0b647
ck <ck@novareto.de>
2014-01-29 11:41:14 +0000

    add grola logic to kursteilnehmer creation

--- src/fernlehrgang/scripts/kursteilnehmer.py
+++ src/fernlehrgang/scripts/kursteilnehmer.py
@@ -21,6 +21,7 @@ from z3c.saconfig import Session
 from fernlehrgang.models import Fernlehrgang, Kursteilnehmer, Teilnehmer, Unternehmen
 from fernlehrgang.interfaces.teilnehmer import generatePassword
 from fernlehrgang import log
+from sqlalchemy import and_
 
 
 def parse_options():
@@ -88,6 +89,7 @@ def main(argv=None):
     app = root[options.portal]
     
     # Logik
+    mto = 0 
     tids = []
     err = []
     session = Session()
@@ -96,7 +98,18 @@ def main(argv=None):
     #import pdb; pdb.set_trace() 
     for i, line in enumerate(DictReader(open(options.file, 'r'), delimiter=";")):
         if z < 19820:
-            unternehmen = Session.query(Unternehmen).get(line['MNR'].strip().replace('-', ''))
+            MNR = line['MNR'].strip().replace('-', '')
+            if len(MNR) == 8:
+                unternehmen = Session.query(Unternehmen).filter(and_(Unternehmen.mnr_g_alt == MNR, Unternehmen.mnr == Unternehmen.mnr_e)).all()
+                if unternehmen:
+                    if len(unternehmen) == 1:
+                        unternehmen = unternehmen[0]
+                    else:
+                        import pdb; pdb.set_trace() 
+
+                print "ACHTUNG GROLA"
+            else:    
+                unternehmen = Session.query(Unternehmen).get(MNR)
             i+=1
             #print '%s, %s %s' %(i, line['MNR'], unternehmen)
             if z in range(0, 20000, 1000):
@@ -109,8 +122,8 @@ def main(argv=None):
                         teilnehmer.name = line['Name'].strip().decode('iso-8859-15')
                     if 'Vorname' in line.keys():
                         teilnehmer.vorname = line['Vorname'].strip().decode('iso-8859-15')
-                    if 'Anrede' in line.keys():
-                        teilnehmer.anrede = line['Anrede'].strip()
+                    #if 'Anrede' in line.keys():
+                    #    teilnehmer.anrede = line['Anrede'].strip()
                     unternehmen.teilnehmer.append(teilnehmer)
                     session.flush()
                     kursteilnehmer = Kursteilnehmer(teilnehmer_id = teilnehmer.id, 
@@ -120,6 +133,7 @@ def main(argv=None):
                     z += 1
                 else:
                     print "Schon mehr als 1 Teilnehmer --> %s" % line['MNR']
+                    mto += 1
             else:
                 #print "Kein Unternehmen gefunden --> %s" % line['MNR']
                 err.append(line['MNR'])
@@ -130,3 +144,8 @@ def main(argv=None):
         print x+','
     print "#" * 50
     print tids
+    print "STATISTIK"
+    print "GESAMT", i
+    print "OK", len(tids)
+    print "MEHR ALS 0", mto
+    print "Kein Unternehmen", len(err)
