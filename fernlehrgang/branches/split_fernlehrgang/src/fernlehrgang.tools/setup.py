bbeb84367990b5503520cce0750db9dcc50be678
trollfot <trollfot@d44c204c-eeda-11de-b92f-5d3588262692>
2014-01-30 14:39:31 +0000

    Added New empty package

---
+++ fernlehrgang/branches/split_fernlehrgang/src/fernlehrgang.tools/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='fernlehrgang.tools',
+      version=version,
+      description="Fernlehrgang Tools and routines",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      license='ZPL',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      namespace_packages=['fernlehrgang'],
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'SQLAlchemy',
+          'zope.interface',
+          'cromlech.file',
+          ],
+      )
