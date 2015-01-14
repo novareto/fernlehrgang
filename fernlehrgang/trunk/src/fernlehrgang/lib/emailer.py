135482f7ac59da80ef98fbbddcfd3c5fdb22d3e9
ck@novareto.de <ck@novareto.de@d44c204c-eeda-11de-b92f-5d3588262692>
2015-01-14 09:46:14 +0000

    make email transaction aware

--- fernlehrgang/trunk/src/fernlehrgang/lib/emailer.py
+++ fernlehrgang/trunk/src/fernlehrgang/lib/emailer.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import zope.sendmail
+import grok
+import zope.component
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
@@ -8,6 +11,27 @@ from email.MIMEText import MIMEText
 from email.Utils import COMMASPACE, formatdate
 from email import Encoders
 from email.header import Header
+
+
+queue_path = "/root/fernlehrgang/test/var/mq"
+
+mailer_object = zope.sendmail.mailer.SMTPMailer('192.168.2.200', 25, force_tls=False)
+
+
+def mailer():
+    return mailer_object
+
+
+def delivery():
+    return zope.sendmail.delivery.QueuedMailDelivery(queue_path)
+
+
+def start_processor_thread():
+    thread = zope.sendmail.queue.QueueProcessorThread()
+    thread.setMailer(mailer_object)
+    thread.setQueuePath(queue_path)
+    thread.start()
+
 
 
 def send_mail(send_from, send_to, subject, text, files=[], server="mail.bghw.de"):
@@ -36,8 +60,24 @@ def send_mail(send_from, send_to, subject, text, files=[], server="mail.bghw.de"
             'attachment; filename="%s"' % os.path.basename(f))
         msg.attach(part)
 
-    smtp = smtplib.SMTP(server)
-    smtp.sendmail(send_from, send_to, msg.as_string())
-    smtp.close()
+    #smtp = smtplib.SMTP(server)
+    mailer = zope.component.getUtility(
+        zope.sendmail.interfaces.IMailDelivery,
+        name=u'flg.maildelivery'
+        )
+    mailer.send(send_from, send_to, msg.as_string())
+
+    #smtp.sendmail(send_from, send_to, msg.as_string())
+    #smtp.close()
+
+grok.global_utility(
+    mailer,
+    provides=zope.sendmail.interfaces.IMailer,
+    name='flg.smtpmailer')
+grok.global_utility(
+    delivery,
+    zope.sendmail.interfaces.IMailDelivery,
+    name='flg.maildelivery')
+start_processor_thread()
 
 #send_mail('fernlehrgang@bghw.de', ('ck@novareto.de',), 'bb', 'bb', server="mail.bghw.de")
