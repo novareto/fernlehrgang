f950bd18bb7f476966d03f4f961153c92b8a6542
goschtl <ck@novareto.de>
2016-10-11 15:14:24 +0200

    chagnes

--- src/fernlehrgang/ftests/test_doctests.py
+++ src/fernlehrgang/ftests/test_doctests.py
@@ -2,21 +2,22 @@
 # Copyright (c) 2007-2010 NovaReto GmbH
 # cklinger@novareto.de
 
-import fernlehrgang
+import ZODB.ActivityMonitor
+import ZODB.interfaces
 import doctest
-import unittest
-import os
-
-from zope.app.testing.functional import ZCMLLayer
-
+import fernlehrgang
 import gocept.httpserverlayer.wsgi
 import gocept.httpserverlayer.zopeappwsgi
-import zope.app.appsetup.testlayer
+import os
+import transaction
+import unittest
 
-from StringIO import StringIO
-import copy
-import plone.testing
-import zope.app.appsetup.product
+from z3c.saconfig import Session
+from zope import component
+from zope.app.appsetup.testlayer import createTestDB
+from zope.app.publication.zopepublication import ZopePublication
+from zope.app.testing.functional import FunctionalTestSetup
+from zope.testing import doctestcase
 
 
 pc = """
@@ -26,7 +27,7 @@ pc = """
 </product-config>
 
 <product-config mailer>
-    queue-path /Users/ck/work/bghw/fernlehrgang/var/mailer-queue
+    queue-path /tmp/mailer-queue
     hostname localhost
     port 25
     username
@@ -40,138 +41,99 @@ ftesting_zcml = os.path.join(
 )
 
 
-class FLGZCMLLayer(ZCMLLayer):
+class ZODBLayer(object):
+    """This layer load a ZCML configuration and create a test database.
 
-    def testSetUp(self):
-        from z3c.saconfig import Session
-        session = Session()
-        from fernlehrgang.models import Base
-        mt = Base.metadata
-        mt.bind = session.connection().engine
-        mt.create_all()
-
-    def testTearDown(self):
-        from fernlehrgang.models import Base
-        mt = Base.metadata
-        mt.drop_all()
-
-FunctionalLayer = FLGZCMLLayer(
-    ftesting_zcml, __name__,
-    'FunctionalLayer',
-    allow_teardown=True,
-    product_config=pc,
-)
-
-from z3c.saconfig import Session
-class ProductConfigLayer(plone.testing.Layer):
-
-    def __init__(self, product_config, **kw):
-        super(ProductConfigLayer, self).__init__(**kw)
+    You can access the test database with layer.getRootFolder().
+    """
+    db = None
+    db_name = 'main'
+    connection = None
+    __bases__ = tuple()
+    
+    def __init__(self, config_file, module, name, allow_teardown=False,
+                 product_config=None):
+        self.config_file = config_file
+        self.__module__ = module
+        self.__name__ = name
+        self.allow_teardown = allow_teardown
         self.product_config = product_config
+    
+    def getRootFolder(self):
+        """This return the root object of the database or assert if
+        the database have not been created yet.
+        """
+        if self.connection is None:
+            assert self.db is not None
+            self.connection = self.db.open()
+        return self.connection.root()[ZopePublication.root_name]
+
+    def _close_db(self):
+        # Close any opened connections
+        if self.connection is not None:
+            transaction.abort()
+            self.connection.close()
+            self.connection = None
+
+        # Close the Database
+        if self.db is not None:
+            base = component.getGlobalSiteManager()
+            base.unregisterUtility(
+                self.db, ZODB.interfaces.IDatabase, self.db_name)
+            self.db.close()
+            self.db = None
 
     def setUp(self):
-        self['old_product_config'] = copy.deepcopy(
-            zope.app.appsetup.product.saveConfiguration())
-        self['product_config'] = zope.app.appsetup.product.loadConfiguration(
-            StringIO(self.product_config))
-        self.testSetUp()
+        self.setup = FunctionalTestSetup(
+            self.config_file, product_config=self.product_config)
+        self.db = createTestDB(self.db_name)
+        self.base_storage = self.db._storage
+        self._base_db_open = True
 
     def testSetUp(self):
-        # Create a copy so tests can modify it as they please.
-       zope.app.appsetup.product.restoreConfiguration(
-           copy.deepcopy(self['product_config']))
+        from fernlehrgang.models import Base
+        session = Session()
+        Base.metadata.create_all(session.connection().engine)
+        transaction.commit()
+        if self._base_db_open:
+            self._close_db()
+            self._base_db_open = False
+        self.db = createTestDB(self.db_name, self.base_storage)
+
+    def testTearDown(self):
+        self._close_db()
+        from fernlehrgang.models import Base
+        session = Session()
+        Base.metadata.drop_all(session.connection().engine)
+        transaction.commit()
+        session.close()
 
     def tearDown(self):
-        del self['product_config']
-        zope.app.appsetup.product.restoreConfiguration(
-            self['old_product_config'])
-        del self['old_product_config']
+        self.setup.tearDownCompletely()
+        if not self.allow_teardown:
+            raise NotImplementedError
 
 
-ZODB_LAYER = zope.app.appsetup.testlayer.ZODBLayer(
-   fernlehrgang, ftesting_zcml)
+LAYER = ZODBLayer(ftesting_zcml, "fernlehrgang", 'layer', product_config=pc)
 
 
 class WSGILayer(gocept.httpserverlayer.zopeappwsgi.Layer):
 
-    defaultBases = (ZODB_LAYER,)
+    defaultBases = (LAYER,)
 
     def get_current_zodb(self):
-        return ZODB_LAYER.db
+        return LAYER.db
 
-WSGI_LAYER = WSGILayer()
 
 HTTP_LAYER = gocept.httpserverlayer.wsgi.Layer(
-    name='HTTPLayer', bases=(WSGI_LAYER,))
+    name='HTTPLayer', bases=(WSGILayer(),))
 
 
-PRODUCT_LAYER = ProductConfigLayer(pc)
-
-
-class AppLayer(plone.testing.Layer):
-    defaultBases = (PRODUCT_LAYER, HTTP_LAYER)
-
-
-APP_LAYER = AppLayer()
-
-APP_LAYER = plone.testing.Layer((PRODUCT_LAYER, HTTP_LAYER), name="app_layer")
-
-#def test_suite():
-#    suite = unittest.TestSuite()
-#    functional = FunctionalDocFileGSuite(
-#        'ftests/ablauf.txt', 'ftests/models.txt', 'ftests/vlw.txt', # 'ftests/accept.txt',
-#        package="fernlehrgang",
-#        globs={'__name__': 'fernlehrgang'},
-#        optionflags=doctest.ELLIPSIS |
-#                    doctest.IGNORE_EXCEPTION_DETAIL |
-#                    doctest.REPORT_NDIFF |
-#                    doctest.NORMALIZE_WHITESPACE,
-#    )
-#    functional.layer = APP_LAYER 
-#    suite.addTest(functional)
-#    return suite
-
-from zope.testing import doctestcase
-from z3c.saconfig import Session
-
-
-@doctestcase.doctestfiles('vlw.txt', 'ablauf.txt', 'models.txt', optionflags=doctest.ELLIPSIS)
+@doctestcase.doctestfiles(
+    'models.txt', 'vlw.txt', 'ablauf.txt', optionflags=doctest.ELLIPSIS)
 class MoreTests(unittest.TestCase):
-    layer = APP_LAYER
+    layer = HTTP_LAYER
 
     def setUp(self):
-        self.globs = {'getRootFolder': ZODB_LAYER.getRootFolder }
+        self.globs = {'getRootFolder': LAYER.getRootFolder }
         self.session = Session()
-        from fernlehrgang.models import Base
-        mt = Base.metadata
-        mt.bind = self.session.connection().engine
-        mt.create_all(checkfirst=True)
-
-    def VVVVtestSetUp(self):
-        print "testSetUp"
-        from fernlehrgang.models import Base
-        mt = Base.metadata
-        mt.bind = self.session.connection().engine
-        mt.create_all(checkfirst=True)
-
-    def tearDown(self):
-        print "TEAR DOWN"
-        from fernlehrgang.models import Base
-        meta = Base.metadata
-        meta.bind = self.session.connection().engine
-        meta.drop_all()
-
-#        with contextlib.closing(self.session.connection()) as con:
-#            trans = con.begin()
-#            for table in reversed(meta.sorted_tables):
-#                con.execute(table.delete())
-#            trans.commit()
-
-
-
-#@doctestcase.doctestfiles('vlw.txt', optionflags=doctest.ELLIPSIS)
-#class MoreTests11(unittest.TestCase):
-#    layer = APP_LAYER
-#    def setUp(self):
-#        self.globs = {'getRootFolder': ZODB_LAYER.getRootFolder }
-
